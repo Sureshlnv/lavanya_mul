eb0a8c77a77b9963d9df5517078e1e5c1d636f48
JaswanthAbbineni <jaswanth.abbineni@gmail.com>
2018-07-04 11:57:49 +0530

    2nd commit

---
+++ src/lavanya_sum/Lavanya_mul.java
@@ -0,0 +1,16 @@
+package lavanya_sum;
+
+public class Lavanya_mul {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a=10,b=35;
+		int mul=a*b;
+		System.out.println(mul);
+	}
+	
+
+	}
+
+
